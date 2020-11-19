c008d5a6e9387ebe2dbc1442fbbf035de8bc3f4b
aderahman007 <aderahman9908@gmail.com>
2020-11-20 03:54:02 +0700

    tambah detail kode booking

---
+++ app/src/main/java/com/medklik/medklik/DetailKodeBookingActivity.java
@@ -0,0 +1,14 @@
+package com.medklik.medklik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailKodeBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_kode_booking);
+    }
+}
