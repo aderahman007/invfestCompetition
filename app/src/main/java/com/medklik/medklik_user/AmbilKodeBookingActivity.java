2be993c9311fbb3b9ce232782059552e5228a555
aderahman007 <aderahman9908@gmail.com>
2020-11-26 14:28:38 +0700

    Complate Layout

---
+++ app/src/main/java/com/medklik/medklik_user/AmbilKodeBookingActivity.java
@@ -0,0 +1,14 @@
+package com.medklik.medklik_user;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AmbilKodeBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ambil_kode_booking);
+    }
+}
---
+++ app/src/main/java/com/medklik/medklik_user/DetailApotekActivity.java
@@ -0,0 +1,14 @@
+package com.medklik.medklik_user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailApotekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_apotek);
+    }
+}
